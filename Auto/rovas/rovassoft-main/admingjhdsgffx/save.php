f76adbab1dc0b96ba5a4239a556ffa8d40e1fe8d
ROVAS Deploy <deploy@rovas.com>
2026-05-01 10:53:23 +0000

    update: remplacer rovassoft-main par la nouvelle version (jeux mis à jour)

--- Auto/rovas/rovassoft-main/admingjhdsgffx/save.php
+++
@@ -1,9 +0,0 @@
-<?php
-$data = file_get_contents('php://input');
-if($data){
-    file_put_contents('games.json', $data);
-    echo json_encode(['status'=>'success']);
-} else {
-    echo json_encode(['status'=>'error', 'message'=>'No data received']);
-}
-?>
